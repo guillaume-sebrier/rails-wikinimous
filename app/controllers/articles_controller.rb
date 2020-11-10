519ee3254b29b527f677842e892a9675cabac93f
guillaume sebrier <guillaume.sebrier@gmail.com>
2020-11-10 14:59:51 +0100

    init

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+
+
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/edit'
+  get 'articles/destroy'
+  get 'articles/update'
+  get 'articles/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
